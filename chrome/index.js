e962a255000495fe49e928505a7d7cae5d83369d
h3d-longnsp <hydroxyzine@metacity.email>
2023-03-23 14:52:06 +0700

    Create render function

--- chrome/index.js
+++ chrome/index.js
@@ -1,17 +1,17 @@
-let mySave = ["sdxz", "sgsg", "sgww34"]
+let myItems = []
 const ulEl = document.getElementById("ul-el")
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 
 inputBtn.addEventListener("click", function() {
-    mySave.push(inputEl.value)
-    console.log(mySave)
+    myItems.push(inputEl.value)
+    renderItems()
 })
 
-let listItems = ""
-for (let i=0; i < mySave.length; i++) {
-    listItems += "<li>" + mySave[i] + "</li>"
+function renderItems() {
+    let listItems = ""
+    for (let i=0; i < myItems.length; i++) {
+        listItems += "<li>" + myItems[i] + "</li>"
+    }
+    ulEl.innerHTML = listItems
 }
-
-ulEl.innerHTML = listItems
-
